fb7e5a87cad319562ce7957234d0893a9195f0b5
Joseph Akhenda <akhenda@gmail.com>
2016-10-13 12:02:07 +0300

    Changed speed variable from a static variable to an instance variable

--- car-test/cartest.py
+++ car-test/cartest.py
@@ -1,43 +1,42 @@
 class Car(object):
-	"""
-		Car class that instantiates various vehicle types
-		
-		Attributes:
-			type: The type of vehicle as a string
-			model: The model of the car as a string
-			name: String representing the name of the car
-	
-	"""
+    """
+    	Car class that instantiates various vehicle types
+    	
+    	Attributes:
+    		type: The type of vehicle as a string
+    		model: The model of the car as a string
+    		name: String representing the name of the car
 
-	speed = 0
-	
-	def __init__(self, name = "General", model = "GM", type = "saloon"):
-		self.type = type
-		self.model = model
-		self.name = name
-	
-	@property
-	def num_of_doors(self):
-		if self.name == "Porshe" or self.name == "Koenigsegg":
-			return 2
-		return 4
+    """
 
-	@property
-	def num_of_wheels(self):
-		if self.type != "trailer":
-			return 4
-		return 8
+    def __init__(self, name = "General", model = "GM", type = "saloon"):
+        self.type = type
+        self.model = model
+        self.name = name
+        self.speed = 0
 
-	def is_saloon(self):
-		if self.type == "saloon":
-			return True
-		return False
+    @property
+    def num_of_doors(self):
+        if self.name == "Porshe" or self.name == "Koenigsegg":
+            return 2
+        return 4
 
-	def drive(self, i=None):
-		if self.name == "Mercedes" or self.type == "saloon":
-			self.speed = 1000
-		elif self.type == "trailer" or self.type == "trailer":
-			self.speed = 77
-		else:
-			self.speed = 72 # Just making sure we change the speed as long as the car is being driven
-		return self
+    @property
+    def num_of_wheels(self):
+        if self.type != "trailer":
+            return 4
+        return 8
+
+    def is_saloon(self):
+        if self.type == "saloon":
+            return True
+        return False
+
+    def drive(self, i):
+        if (self.name == "Mercedes" or self.type == "saloon") and i == 3:
+            self.speed = 1000
+        elif (self.type == "trailer" or self.type == "trailer") and i == 7:
+            self.speed = 77
+        else:
+            self.speed = 72 # Just making sure we change the speed as long as the car is being driven
+        return self
